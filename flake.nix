5a2b60346d89d3c47b7a09490276d56f81a19a47
Silmar <silmarjr2@gmail.com>
2023-05-09 16:18:04 -0300

    add venv to flake

--- flake.nix
+++ flake.nix
@@ -2,19 +2,13 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/master";
     flake-utils.url = "github:numtide/flake-utils";
-    # TODO Change to using dream2nix, mach-nix is now unmantained
-    mach-nix.url = "mach-nix/3.5.0";
   };
 
-  outputs = {self, nixpkgs, flake-utils, mach-nix }@inp:
+  outputs = {self, nixpkgs, flake-utils, }@inp:
   flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
 
-        pythonEnv = mach-nix.lib.${system}.mkPython {
-          python = "python310";
-          requirements = builtins.readFile ./src/requirements.txt;
-        };
         devPkgs = with pkgs; [
           nodePackages.pyright
         ];
@@ -22,9 +16,16 @@
       {
         devShell = pkgs.mkShell {
           nativeBuildInputs = [
-            pythonEnv
+            pkgs.python310
+            pkgs.python310Packages.pip
+            pkgs.python310Packages.virtualenv
             devPkgs
           ];
+          /*
+          shellHook = ''
+            source ./VENV/bin/activate
+          '';
+          */
         };
       });
 }
