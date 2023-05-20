0b87ff0630e5d11c4c29e3117584fac382ca0cb3
mRlFranchi <murilo.franchi@usp.br>
2023-05-20 18:37:23 -0300

    added dist method and changed add_neighbor to add_edge

--- src/vertex.py
+++ src/vertex.py
@@ -5,11 +5,11 @@ class Vertex:
         self._y = y
         self._neighbors = []
     
-    def add_neighbor(self,n_vertex, weight = 0, bidirectional = False):
+    def add_edge(self, n_vertex, weight = 0, bidirectional = False):
         if self not in n_vertex.get_neighbors():
             self._neighbors.append((n_vertex, weight))
         if bidirectional:
-            n_vertex.add_neighbor(self)
+            n_vertex.add_edge(self)
     
     def get_neighbors(self):
         return self._neighbors
@@ -23,6 +23,9 @@ class Vertex:
     def get_label(self):
         return self._label
     
+    def dist(self,vertex):
+        from math import dist
+        return dist([self.get_x(),self.get_y()],[vertex.get_x(),vertex.get_y()])
     
     def __str__(self):
         return (self._label)
